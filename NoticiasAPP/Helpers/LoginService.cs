53b119780dc697ae162aacf4e771caa79bb7498b
CarlosCuautleZac <191g0235@rcarbonifera.tecnm.mx>
2023-06-06 01:37:43 -0600

    Services em maui

---
+++ NoticiasAPP/Helpers/LoginService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoticiasAPP.Helpers
+{
+    public class LoginService
+    {
+        private readonly AuthService auth;
+        public string url = "https://seguridaddocentes.itesrc.net/";
+        HttpClient client;
+        public LoginService(AuthService auth)
+        {
+            //Le paso la url al httpclient
+            client = new HttpClient()
+            {
+                BaseAddress = new Uri(url)
+            };
+            this.auth = auth;
+
+
+        }
+
+        public async Task<bool> IniciarSesion(LoginDTO login)
+        {
+            if (string.IsNullOrWhiteSpace(login.Usuario) || string.IsNullOrWhiteSpace(login.Contraseña))
+            {
+                throw new ArgumentException("Escriba el nombre de usuario o contraseña");
+            }
+
+            var request = await client.PostAsJsonAsync("api/login", login);
+            if (request.IsSuccessStatusCode)
+            {
+
+                //read token
+                var token = await request.Content.ReadAsStringAsync();
+
+                auth.WriteToken(token);
+                return true;
+            }
+            else
+            {
+                var message = request.Content.ReadAsStringAsync();
+                return false;
+            }
+        }
+
+        public async void Logout()
+        {
+            auth.RemoveToken();
+            await Shell.Current.GoToAsync("login");
+        }
+    }
+
+}
---
+++ NoticiasAPP/Models/NoticiaDTO.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoticiasAPP.Models
+{
+    public class NoticiaDTO
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = "";
+        public string Autor { get; set; } = "";
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; } = "";
+        public string Categoria { get; set; } = "";
+
+        //Cuando sea para get se mandara un url de la imagen de la noticia que esta en la api, cuando sea post se enviara un base64
+        public string Imagen { get; set; } = "";
+
+        public string ImagenAutor { get; set; } = "";
+
+        //Estas propiedades solo se usara cuando se haga un post
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public int IdAutor { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public int IdCategoria { get; set; }
+    }
+}
