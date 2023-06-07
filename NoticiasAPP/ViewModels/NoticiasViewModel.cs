7ab51a6f4c7a6f764ca9081e63c450a5ec6e7e6c
CarlosCuautleZac <191g0235@rcarbonifera.tecnm.mx>
2023-06-06 23:14:55 -0600

    Avances en viewmodel de noticiass

--- NoticiasAPP/ViewModels/NoticiasViewModel.cs
+++ NoticiasAPP/ViewModels/NoticiasViewModel.cs
@@ -10,16 +10,21 @@ namespace NoticiasAPP.ViewModels
 {
     public class NoticiasViewModel : INotifyPropertyChanged
     {
+        //Campos de services
         private readonly LoginService login;
 
+        //Comandos
         public Command CerrarSesionCommand { get; set; }    
 
+
+        //Constructor
         public NoticiasViewModel(LoginService login)
         {
             this.login = login;
             CerrarSesionCommand = new Command(CerrarSesion);
         }
 
+        //Metodos
         private void CerrarSesion()
         {
             if (Connectivity.Current.NetworkAccess == NetworkAccess.Internet)
