9b35bd36db69eedec0f6a20feb0e2e698d1c2025
Angel Villafán <miguelangel9505@hotmail.com>
2023-06-12 00:54:31 -0600

    cambios a vistas

--- NoticiasAPP/Views/MainPage.xaml.cs
+++ NoticiasAPP/Views/MainPage.xaml.cs
@@ -16,12 +16,37 @@
         {
             border.TranslationX = 0;
             border.TranslationY = 750;
+
+
+            borderregistrar.TranslationX = 0;
+            borderregistrar.TranslationY = 750;
         }
 
         private void Button_Pressed(object sender, EventArgs e)
         {
             grid.FadeTo(0.6, 500);
             border.TranslateTo(0, 0, 500, Easing.SinInOut);
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        {
+            grid.FadeTo(0.6, 500);
+            borderregistrar.TranslateTo(0, 0, 500, Easing.SinInOut);
+        }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, TappedEventArgs e)
+        {
+            grid.FadeTo(0, 500);
+            border.TranslationX = 0;
+            border.TranslationY = 750;
+        }
+
+        private void TapGestureRecognizer_Tapped_2(object sender, TappedEventArgs e)
+        {
+            grid.FadeTo(0, 500);
+
+            borderregistrar.TranslationX = 0;
+            borderregistrar.TranslationY = 750;
         }
     }
 }
